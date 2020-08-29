2617f242a89fae8d0265fe05b5fcf3a7125217b3
Silva Spencer <68076204+Silva2245@users.noreply.github.com>
2020-08-29 05:53:14 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Sev;
+using Reen;
+using System.Text;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Linq;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Sevreen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try{
+                Console.WriteLine();
+                Console.Write("ENTER PRIMARY CODE : ");
+                string pc = Console.ReadLine();
+            }
+            catch (Exception ex){
+                Console.WriteLine();
+                Console.WriteLine("THE PROBLEM : " + ex.Message.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
